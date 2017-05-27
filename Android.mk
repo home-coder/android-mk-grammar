b8c19811899b02112eeea0b80c54aae1f41a5796
jiangxiujie <jiangxiujie@cantv.cn>
2017-05-27 17:29:59 +0800

    添加一些需要实现的常见的编译系统内部定义的 宏

--- Android.mk
+++ Android.mk
@@ -6,4 +6,32 @@ LOCAL_MODULE:= yang
 
 LOCAL_SRC_FILES := yang.c
 
+
+#call
+
+#ifneq
+
+#include
+
+#ifeq ($(strip $(BOARD_ALSA_AUDIO)),tiny)
+
+#LOCAL_PATH := $(call my-dir) -->冒号等于
+
+#LOCAL_C_INCLUDES -->为什么要有include
+
+#LOCAL_MODULE_TAGS
+
+#LOCAL_SRC_FILES
+
+#LOCAL_SHARED_LIBRARIES
+
+#LOCAL_MODULE
+#include $(call all-makefiles-under,$(LOCAL_PATH))
+
+#include $(BUILD_SHARED_LIBRARY)
+
+#LOCAL_CFLAGS
+
+#...
+
 include $(BUILD_SHARED_LIBRARY)
